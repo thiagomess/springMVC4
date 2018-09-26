23d63fe5b5ba5c62e79b62663335fa9bf388c8f6
Thiago Gomes <thiagogomes19@hotmail.com>
2018-09-26 11:04:34 -0300

    Inicio projeto

---
+++ src/br/com/caelum/contas/ConnectionFactory.java
@@ -0,0 +1,23 @@
+package br.com.caelum.contas;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	
+	public Connection getConnection() throws SQLException {
+		System.out.println("conectando ...");
+
+		try {
+			Class.forName("org.hsqldb.jdbcDriver");
+			System.out.println("conectou");
+		} catch (ClassNotFoundException e) {
+			throw new SQLException(e);
+		}
+		
+		return DriverManager.getConnection("jdbc:hsqldb:file:contas.db","sa", "");
+	}
+
+}
