b78b06ee41c28439d540c66296d35978fbf7037f
Thiago Gomes <thiagogomes19@hotmail.com>
2018-09-27 15:58:56 -0300

    Efetuando lista na tela podendo utilizar o ModelAndView ou o Model por paramentro

--- src/br/com/caelum/contas/controller/ContaController.java
+++ src/br/com/caelum/contas/controller/ContaController.java
@@ -1,7 +1,11 @@
 package br.com.caelum.contas.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.contas.dao.ContaDAO;
 import br.com.caelum.contas.modelo.Conta;
@@ -22,4 +26,28 @@ public class ContaController {
 	    dao.adiciona(conta);
 	    return "conta/conta-adicionada";
 	  }
+	
+	
+//	Para mandar para a JSP deve ser retorno ModelAndView Pacote org.springframework.web.servlet.ModelAndView;
+	@RequestMapping("/listaContas")
+	public ModelAndView lista() {
+		
+		ContaDAO dao = new ContaDAO();
+		List<Conta> contas = dao.lista();
+		ModelAndView mv = new ModelAndView("conta/lista"); //Adiciona o caminho da pagina no construtor
+		mv.addObject("todasContas", contas); //adiciona a lista
+		
+		return mv;
+		
+	}
+	
+// A lista tambem pode ser feita desse modo, onde recebemos o Model no metodo como param	
+//	@RequestMapping("/listaContas")
+//	public String lista(Model mv) {
+//	  ContaDAO dao = new ContaDAO();
+//	  List<Conta> contas = dao.lista();
+//
+//	  mv.addAttribute("todasContas", contas);
+//	  return "conta/lista";
+//	}
 }
